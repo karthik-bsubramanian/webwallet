8b6c10c939bf52c99de0c3015bad780e0e460d59
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-08-31 01:57:05 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      
-    </div>        
+    <div className="w-full h-[800px] flex items-center justify-center gap-4 flex-col">
+      <h1 className="text-5xl font-extrabold">BabyBoy Supports Multiple Blockchains</h1>
+      <h2 className="text-2xl">Chose a Chain and Get Started</h2>
+      <div className="flex gap-3">
+        <Button variant="default" className="text-xl cursor-pointer">Solana</Button>
+        <Button variant="default" className="text-xl cursor-pointer">Ethereum</Button>
+      </div>
+    </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 import Image from "next/image"
-import { Button } from "./ui/button"
 import { ThemeToggle } from "./Theme-toggle"
 
 export const Header = () => {
     return <div className="h-20 w-full flex justify-between items-center">
+        <div className="flex gap-2 items-center">
         <Image
             src="/icon.png"
             alt="Logo"
             width={40}
             height={40}
         />
+        <span className="text-2xl">BabyBoy</span>
+        </div>
         <ThemeToggle/>
     </div>
 }
